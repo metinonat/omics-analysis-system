8601d363c59df92688c761e99ab3cbe5dd27fce5
metin onat <44121642+metinonat@users.noreply.github.com>
2023-10-13 14:26:54 +0300

    fix: unknown error commented out

--- frontend/src/components/analysis/BoxPlot.tsx
+++ frontend/src/components/analysis/BoxPlot.tsx
@@ -1,86 +1,91 @@
+"use client";
+import dynamic from "next/dynamic";
 import React from "react";
-import ReactApexChart from "react-apexcharts";
 import MultiSelect from "../common/MultiSelect";
 
-interface ApexChartProps {
-  series: any[];
-}
+// interface ApexChartProps {
+//   series: any[];
+// }
 
-interface ApexChartState {
-  series: any[];
-  options: {
-    chart: {
-      type: string;
-      height: number;
-    };
-    colors: string[];
-    title: {
-      text: string;
-      align: string;
-    };
-    xaxis: {
-      type: string;
-      tooltip: {
-        formatter: (val: string) => string;
-      };
-    };
-    tooltip: {
-      shared: boolean;
-      intersect: boolean;
-    };
-  };
-}
+// interface ApexChartState {
+//   series: any[];
+//   options: {
+//     chart: {
+//       type: string;
+//       height: number;
+//     };
+//     colors: string[];
+//     title: {
+//       text: string;
+//       align: string;
+//     };
+//     xaxis: {
+//       type: string;
+//       tooltip: {
+//         formatter: (val: string) => string;
+//       };
+//     };
+//     tooltip: {
+//       shared: boolean;
+//       intersect: boolean;
+//     };
+//   };
+// }
 
-class ApexChart extends React.Component<ApexChartProps, ApexChartState> {
-  constructor(props: ApexChartProps) {
-    super(props);
+// class ApexChart extends React.Component<ApexChartProps, ApexChartState> {
+//   constructor(props: ApexChartProps) {
+//     super(props);
 
-    this.state = {
-      series: props.series,
-      options: {
-        chart: {
-          type: "boxPlot",
-          height: 350,
-        },
-        colors: ["#008FFB", "#FEB019"],
-        title: {
-          text: "BoxPlot - Scatter Chart",
-          align: "left",
-        },
-        xaxis: {
-          type: "string",
-          tooltip: {
-            formatter: function (val: string) {
-              return val;
-            },
-          },
-        },
-        tooltip: {
-          shared: false,
-          intersect: true,
-        },
-      },
-    };
-  }
-  render() {
-    return (
-      <div id="chart">
-        <ReactApexChart
-          id="apexchart"
-          options={this.state.options as any}
-          series={this.state.series}
-          type="boxPlot"
-          height={350}
-        />
-      </div>
-    );
-  }
-}
+//     this.state = {
+//       series: props.series,
+//       options: {
+//         chart: {
+//           type: "boxPlot",
+//           height: 350,
+//         },
+//         colors: ["#008FFB", "#FEB019"],
+//         title: {
+//           text: "BoxPlot - Scatter Chart",
+//           align: "left",
+//         },
+//         xaxis: {
+//           type: "string",
+//           tooltip: {
+//             formatter: function (val: string) {
+//               return val;
+//             },
+//           },
+//         },
+//         tooltip: {
+//           shared: false,
+//           intersect: true,
+//         },
+//       },
+//     };
+//   }
+//   render() {
+//     return (
+//       <div id="chart">
+//         <ReactApexChart
+//           id="apexchart"
+//           options={this.state.options as any}
+//           series={this.state.series}
+//           type="boxPlot"
+//           height={350}
+//         />
+//       </div>
+//     );
+//   }
+// }
 export default function BoxPlot() {
   const [data, setData] = React.useState<{ series: any[] }>({ series: [] });
   React.useEffect(() => {
     console.log(data);
   }, [data]);
+  const DynamicApexChart = dynamic(() => import("react-apexcharts"), {
+    ssr: false,
+  });
+
   return (
     <>
       <MultiSelect
@@ -88,7 +93,7 @@ export default function BoxPlot() {
         setData={setData}
         useThreshold={true}
       />
-      <ApexChart series={data.series} />
+      {/* <DynamicApexChart series={data.series} /> */}
     </>
   );
 }
